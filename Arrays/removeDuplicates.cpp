db05a6245ae34712e485dbc661041652a6c27aab
gopal-fs <gopalpinapathuni.fs@gmail.com>
2026-01-27 09:44:11 +0530

    Removing Duplicates in-place in an array

---
+++ Arrays/removeDuplicates.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+      int removeDuplicates(vector& nums) {
+          int x = 0;
+          for (int i = 0; i < nums.size(); i++) {
+              if (nums[i] > nums[x]) {
+                  x++;
+                  nums[x] = nums[i];
+              }
+          }
+          return x + 1;
+      }
+  };
